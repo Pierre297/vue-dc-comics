ccfa3bc6132db7840df506beed05741ee2a22c59
Pier <pierfrancesco297@gmail.com>
2021-11-24 15:38:39 +0100

    macrostruttura pagina

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <Main/>
+    <Footer/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Testata.vue '
+import Main from './components/Principale.vue '
+import Footer from './components/Piepagina.vue '
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Header,
+    Main,
+    Footer,
+  },
 }
 </script>
 
